d0bf2069af7157982659f6dfdc933460d9a8c45d
z2091 <140894537+z2091@users.noreply.github.com>
2023-07-31 19:44:09 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '1998577a-dd79-4da4-8e2e-27a25c3e0fd2';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
